b050bdda9d0be82b9be66b1721fcd3142d92134a
Katie Brown <katieleebrown19@gmail.com>
2022-10-13 14:43:09 -0400

    db changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ const Card = ({ tasks, onDelete, onToggle, onAdd }) => {
     return (
         <div className='container border mt-5 px-4 py-3'>
             <div className='container py-3 px-0 mx-0 d-flex justify-content-between'>
-                <h2>Task Tracker</h2>
+                <h2>My Task Tracker</h2>
                 <Button color={showAddTask ? 'dark' : 'success'} text={showAddTask ? 'Close' : 'Add'} onAdd={() => setshowAddTask(!showAddTask)} />
             </div>
             {showAddTask && <AddTask onAdd={onAdd} />}
